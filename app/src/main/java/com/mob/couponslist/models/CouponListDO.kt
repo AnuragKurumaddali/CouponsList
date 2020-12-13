93f5232a1c4385ab977edc68d8f3448604af087e
Anurag <anurag.k@apalya.com>
2020-12-13 20:27:40 +0530

    * Removed one unwanted Pojo class

--- app/src/main/java/com/mob/couponslist/models/CouponListDO.kt
+++
@@ -1,9 +0,0 @@
-package com.mob.couponslist.models
-
-import com.google.gson.annotations.SerializedName
-
-class CouponListDO {
-
-    @field:SerializedName("visibility_user_levels")
-    val visibilityUserLevels: List<Int?>? = null
-}
